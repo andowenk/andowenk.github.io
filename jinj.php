3a81c76dc393f2bd3e261a83830109d274d00807
andowenk <62374198+andowenk@users.noreply.github.com>
2023-04-28 17:28:20 +0800

    Update jinj.php

--- jinj.php
+++ jinj.php
@@ -1,5 +1,5 @@
 <?php
-$m3u8 = json_decode(file_get_contents('http://www.jinjiang.tv/m2o/channel/channel_info.php?id=4'))[0]->m3u8;
+$m3u8 = json_decode(file_get_contents('http://http://stream.jinjiang.tv/1/sd/channel_info.php?id=4'))[0]->m3u8;
 header('location:'.$m3u8);
 //echo $m3u8;
 ?>
